905e9ea3d343503a2d51236eda18ef1737784b60
ssxbxk <ssxbxk@163.com>
2018-08-17 10:56:13 +0800

    Signed-off-by: ssxbxk <ssxbxk@163.com>

--- pages/index/index.js
+++ pages/index/index.js
@@ -28,23 +28,6 @@ Page({
       url: '../zbzcshistory/zbzcshistory'
     })
   },
-  onClickImage:function(){
-    wx.getSetting({
-      success(res) {
-        if (!res.authSetting['scope.writePhotosAlbum']) {
-          wx.authorize({
-            scope: 'scope.writePhotosAlbum',
-            success() {
-              saveImageToLocal();
-            }
-          })
-        }
-        else{
-          saveImageToLocal();
-        }
-      }
-    })
-  },
   onShareAppMessage: function (e) {
     return {
       title: utils.shareTitle,
@@ -53,20 +36,3 @@ Page({
     }
   }
 })
-
-function saveImageToLocal(){
-  wx.getImageInfo({
-    src: '../../images/ds.png',
-    success: function (ret) {
-      var path = ret.path;
-      wx.saveImageToPhotosAlbum({
-        filePath: path,
-        success(result) {
-          wx.showToast({
-            title: '二维码保存成功!',
-          })
-        }
-      })
-    }
-  })
-}
